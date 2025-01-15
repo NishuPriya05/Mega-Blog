10dd10fb0ecefca57c105cf952162666ecb66b85
Nishu Priya <priyanishu0503@gmail.com>
2025-01-15 14:31:16 +0530

    megablog

--- src/Components/Header/LogoutBtn.jsx
+++ src/Components/Header/LogoutBtn.jsx
@@ -1,12 +1,16 @@
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
+import { useNavigate } from "react-router-dom"; 
 import { logout } from "../../store/authSlice";
 
 function LogoutBtn() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const logoutHandler = () => {
     authService.logout().then(() => {
       dispatch(logout());
+      navigate("/");
     });
   };
   return (
